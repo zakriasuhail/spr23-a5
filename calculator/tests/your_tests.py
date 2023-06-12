e0f9b50f17c79107392c987422b26b3d2781951d
zakriasuhail <73486892+zakriasuhail@users.noreply.github.com>
2023-06-12 16:26:14 -0700

    added Makefile + tests + github.txt

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,8 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
-
+assert run("5 * 10").output == "50"
+assert run("5 * 5").exit_status == 0
 ###
 
 print("All tests passed!")
